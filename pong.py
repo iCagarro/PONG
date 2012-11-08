7a45ddeaa2a6cc0b1a931f09d5ceed1259011b13
Adria Huguet Ferran <adriahuguet@gmail.com>
2012-11-08 20:09:22 +0100

    parts principals del programa en funcions
    aqui aniria una descripcio mes llarga sobre el tema

--- pong.py
+++ pong.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2
 # Implementation of classic arcade game Pong
 
 import pygame, sys
@@ -42,6 +41,11 @@ paddle_im = pygame.image.load(paddle).convert()
 # titol de la finestra
 pygame.display.set_caption('PONG')
 
+### rellotge
+##while True:
+##    clock = pygame.time.get_ticks()
+##    print(clock)
+    
 # funcions
 def ball_init(right):
     global ball_pos, ball_vel
@@ -57,70 +61,20 @@ def ball_init(right):
         ball_vel = [-x, y]
         return ball_pos, ball_vel
 
-ball_init(right)
-
-# LOOP PRINCIPAL
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-                
-    # posicio de la ball (t)
-    ball_pos[0] += ball_vel[0]
-    ball_pos[1] += ball_vel[1]
-
-    # posicio dels paddles
-    paddle1_pos[1] += paddle1_vel
-    paddle2_pos[1] += paddle2_vel
-
-    # paddle limits
-    if paddle1_pos[1] < (HALF_PAD_HEIGHT - 1):
-        paddle1_vel = 0
-        paddle1_pos[1] = HALF_PAD_HEIGHT
-    if paddle1_pos[1] > (HEIGHT - HALF_PAD_HEIGHT + 1):
-        paddle1_vel = 0
-        paddle1_pos[1] = HEIGHT - HALF_PAD_HEIGHT
-    if paddle2_pos[1] < (HALF_PAD_HEIGHT - 1):
-        paddle2_vel = 0
-        paddle2_pos[1] = HALF_PAD_HEIGHT
-    if paddle2_pos[1] > (HEIGHT - HALF_PAD_HEIGHT + 1):
-        paddle2_vel = 0
-        paddle2_pos[1] = HEIGHT - HALF_PAD_HEIGHT
-
-    # walls
-    ball_pos[0] += ball_vel[0]
-    ball_pos[1] += ball_vel[1]
-    if ball_pos[1] <= ball_radius or ball_pos[1] >= (HEIGHT - ball_radius):
-        ball_vel[1] = -ball_vel[1]
-
-    # ball vs pads
-    if ball_pos[0] <= (PAD_WIDTH + ball_radius) and (ball_pos[1] <= (paddle1_pos[1] + HALF_PAD_HEIGHT) and ball_pos[1] >= (paddle1_pos[1] - HALF_PAD_HEIGHT)):
-        ball_vel[0] = abs(ball_vel[0] + ball_vel[0] * 0.1)
-    if ball_pos[0] >= (WIDTH - PAD_WIDTH - ball_radius) and (ball_pos[1] <= (paddle2_pos[1] + HALF_PAD_HEIGHT) and ball_pos[1] >= (paddle2_pos[1] - HALF_PAD_HEIGHT)):
-        ball_vel[0] = -abs(ball_vel[0] + ball_vel[0] * 0.1) 
-
-    # gols
-    if ball_pos[0] < - ball_radius:
-        ball_init(True)
-        score1 += 1
-    if ball_pos[0] > WIDTH + ball_radius:
-        ball_init(False)
-        score2 += 1
-    
-    # teclat
+def key_down():
+    global paddle1_vel, paddle2_vel
     if event.type == KEYDOWN:
         if event.key == K_w:
-            paddle1_vel = -1
+            paddle1_vel = -0.3
         elif event.key == K_s:
-            paddle1_vel = 1
+            paddle1_vel = 0.3
         elif event.key == K_UP:
-            paddle2_vel = -1
+            paddle2_vel = -0.3
         elif event.key == K_DOWN:
-            paddle2_vel = 1
-        elif event.key == K_b:
-            right = True
+            paddle2_vel = 0.3
 
+def key_up():
+    global paddle1_vel, paddle2_vel
     if event.type == KEYUP:
         if event.key == K_w:
             paddle1_vel = 0
@@ -131,13 +85,92 @@ while True:
         elif event.key == K_DOWN:
             paddle2_vel = 0
 
+def key_handle():
+    key_down()
+    key_up()
+
+def paddle_limits():
+    if paddle1_pos[1] <= (HALF_PAD_HEIGHT - 1):
+        paddle1_vel = 0
+        paddle1_pos[1] = HALF_PAD_HEIGHT
+    if paddle1_pos[1] >= (HEIGHT - HALF_PAD_HEIGHT + 1):
+        paddle1_vel = 0
+        paddle1_pos[1] = HEIGHT - HALF_PAD_HEIGHT
+    if paddle2_pos[1] <= (HALF_PAD_HEIGHT - 1):
+        paddle2_vel = 0
+        paddle2_pos[1] = HALF_PAD_HEIGHT
+    if paddle2_pos[1] >= (HEIGHT - HALF_PAD_HEIGHT + 1):
+        paddle2_vel = 0
+        paddle2_pos[1] = HEIGHT - HALF_PAD_HEIGHT
+
+def update_ball():
+    global ball_pos, ball_vel
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+
+def update_paddle():
+    global paddle1_pos, paddle1_vel, paddle2_pos, paddle2_vel
+    paddle1_pos[1] += paddle1_vel
+    paddle2_pos[1] += paddle2_vel
+
+def walls():
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    if ball_pos[1] <= ball_radius or ball_pos[1] >= (HEIGHT - ball_radius):
+        ball_vel[1] = -ball_vel[1]
+
+def ball_vs_pads():
+    if ball_pos[0] < (PAD_WIDTH + ball_radius) and (ball_pos[1] < (paddle1_pos[1] + HALF_PAD_HEIGHT) and ball_pos[1] > (paddle1_pos[1] - HALF_PAD_HEIGHT)):
+        ball_vel[0] = abs(ball_vel[0] + ball_vel[0] * 0.1)
+    if ball_pos[0] > (WIDTH - PAD_WIDTH - ball_radius) and (ball_pos[1] < (paddle2_pos[1] + HALF_PAD_HEIGHT) and ball_pos[1] > (paddle2_pos[1] - HALF_PAD_HEIGHT)):
+        ball_vel[0] = -abs(ball_vel[0] + ball_vel[0] * 0.1) 
+
+def goal():
+    global score1, score2
+    if ball_pos[0] < PAD_WIDTH:
+        ball_init(True)
+        score1 += 1
+    if ball_pos[0] > WIDTH - PAD_WIDTH:
+        ball_init(False)
+        score2 += 1
+
+ball_init(right)
+
+# LOOP PRINCIPAL
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+ 
+    # posicio de la ball (t)
+    update_ball()
+
+    # posicio dels paddles
+    update_paddle()
+
+    # paddle limits
+    paddle_limits()
+
+    # walls
+    walls()
+
+    # ball vs pads
+    ball_vs_pads()
+    
+    # gols
+    goal()
+    
+    # teclat
+    key_handle()
+
     # dibuixar fons
     screen.blit(background, (0, 0))
     # dibuixar paddles
     screen.blit(paddle_im, (paddle1_pos[0] - HALF_PAD_WIDTH, paddle1_pos[1] - HALF_PAD_HEIGHT))
-    screen.blit(paddle_im, (paddle2_pos[0] - HALF_PAD_WIDTH, paddle2_pos[1] - HALF_PAD_HEIGHT))
+    screen.blit(paddle_im, (paddle2_pos[0] - HALF_PAD_WIDTH + 1, paddle2_pos[1] - HALF_PAD_HEIGHT))
     # dibuixar ball
     screen.blit(ball_im, (ball_pos[0] - ball_radius, ball_pos[1] - ball_radius))
     # dibuixar scores
-
     pygame.display.update()
+
